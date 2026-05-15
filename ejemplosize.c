9891d13cc0828a70a6413f665988d6b30607ee43
Luis Orozco <luis.orozco.villacis@udla.edu.ec>
2026-05-15 14:47:20 -0500

    Segunda version del ejercico 2 viendo tamaño de bytes - Luis Orozco

--- ejemplosize.c
+++ ejemplosize.c
@@ -2,6 +2,9 @@
 
 int main (int argc, char *argv[]) {
     int i=sizeof(int);
-    printf("%d bytes", i);
+    printf("%d bytes\n", i);
+    int arreglo[6]={3,6,9,12,15,18};
+    int j=sizeof(arreglo);
+    printf("%d bytes", j);
     return 0;
 }
