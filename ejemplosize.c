7791b39174999310fac3bb866fdd51729a579fd0
Luis Orozco <luis.orozco.villacis@udla.edu.ec>
2026-05-15 14:50:25 -0500

    Tercera version del codigo del ejercicio 2 - Luis Orozco

--- ejemplosize.c
+++ ejemplosize.c
@@ -5,6 +5,15 @@ int main (int argc, char *argv[]) {
     printf("%d bytes\n", i);
     int arreglo[6]={3,6,9,12,15,18};
     int j=sizeof(arreglo);
-    printf("%d bytes", j);
+    printf("%d bytes\n", j);
+    printf("This computing environment uses:\n");
+    printf("%zu byte for chars\n", sizeof(char));
+    printf("%zu bytes for shorts\n", sizeof(short int));
+    printf("%zu bytes for ints\n", sizeof(int));
+    printf("%zu bytes for longs\n", sizeof(long int));
+    printf("%zu bytes for long longs\n", sizeof(long long int));
+    printf("%zu bytes for floats\n", sizeof(float));
+    printf("%zu bytes for doubles\n", sizeof(double));
+    printf("%zu bytes for pointers\n", sizeof(int *));
     return 0;
 }
