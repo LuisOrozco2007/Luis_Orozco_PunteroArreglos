5a4216455196d2163cc88e9264abfe6fa3723d6c
Luis Orozco <luis.orozco.villacis@udla.edu.ec>
2026-05-15 15:19:57 -0500

    Tercera version del ejercicio 3 - Luis Orozco

--- ejercicio3.c
+++ ejercicio3.c
@@ -3,10 +3,11 @@
 int main (int argc, char *argv[]) {
     char vocales[]={'a','e','i','o','u'};
     char *ptr;
-    ptr=vocales;
+    ptr=&vocales[3]; *ptr='x';
+
     for (int i = 0; i < 5; i++)
     {
-        printf("%c ", *(ptr+i));
+        printf("%c ", vocales[i]);
     }
     
     return 0;
